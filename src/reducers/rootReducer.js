25a01480606bd42282a0567950b48e7210f50e8c
Merdan Kurban <namainterior@gmail.com>
2021-05-04 22:06:06 +0400

    Add provider

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+iimport {combineReducers} from 'redux'
+import userReducer from './userReducer'
+import productReducer from './productReducer'
+
+export const rootReducer = 
+combineReducers({ userReducer, productReducer });
